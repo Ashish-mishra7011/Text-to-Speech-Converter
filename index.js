ff113844c76321bf18c33ed6f12eadc434f40aa3
Ashish Mishra <83667639+Ashish-mishra7011@users.noreply.github.com>
2024-08-02 01:15:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
 let voiceselect = document.querySelector("select");
+
 window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
     voices.forEach((voice, i) => {
         voiceselect.options[i] = new Option(voice.name, i);
-    })
-
+    });
 };
-voiceselect.addEventListener("change", () => {
-    speech.voice = voices[voiceselect.value]
-})
 
-document.querySelector("button"), addEventListener("click", () => {
+voiceselect.addEventListener("change", () => {
+    speech.voice = voices[voiceselect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
     if (window.speechSynthesis.speaking) {
-        window.speechSynthesis.cancel(); // Cancel if there is any ongoing speech jisse ki ye repeat na kare
+        window.speechSynthesis.cancel(); // Cancel any ongoing speech
     }
     speech.text = document.querySelector("textarea").value;
     setTimeout(() => {
         window.speechSynthesis.speak(speech);
     }, 100);
-})
+});
